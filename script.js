b02f5e42d46c0b94b1e2f91a67373ca25dd0421e
indigoBill <117851497+indigoBill@users.noreply.github.com>
2023-11-07 09:58:28 -0500

    Fix bugs in custom password validation

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const password = document.querySelector('#password');
 const cnfrm_password = document.querySelector('#confirm_pass');
+const bothPasswords = document.querySelectorAll("[type=password]");
 const error = document.querySelector('.confirm-error');
 const btn = document.querySelector('.btn');
 
-
-
-cnfrm_password.addEventListener('keyup',() => {
-
-    if(password.value !== cnfrm_password.value){
-        error.textContent = '*REQUIRED: PASSWORD SHOULD MATCH';
-        error.classList.add('match-error');
-
-    }else{
+function checkPasswords(e){
+    if(password.value == cnfrm_password.value){
         error.textContent = '';
+    }else{
+        error.textContent = '*REQUIRED: PASSWORDS SHOULD MATCH';
+        error.classList.add('match-error');
     }
-});
+}
+
+bothPasswords.forEach((onePassword) => onePassword.addEventListener('keyup', checkPasswords));
+bothPasswords.forEach((onePassword) => onePassword.addEventListener('focus', checkPasswords));
 
 btn.addEventListener('click', () => {
     if(password.value !== cnfrm_password.value){
